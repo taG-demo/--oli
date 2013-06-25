2092a455301134a80b01360fbdc48f7bc6f842a2
Mr Speaker <mrspeaker@gmail.com>
2013-06-25 22:49:31 +0200

    clean up a bit

--- src/audio.js
+++ src/audio.js
@@ -12,47 +12,48 @@
   		n8 = (60 / tempo) / 2,
   		n16 = n8 / 2,
       	n32 = n16 / 2,
+
 		kickLen = (n8 * 1) * sr,
 		kickBuffer = actx.createBuffer(1, kickLen, sr),
 		kickData = kickBuffer.getChannelData(0),
-		kickNode,
 
-		melLen = (n32 * 1) * sr,
-		melBuf = actx.createBuffer(1, melLen, sr),
-		melData = melBuf.getChannelData(0),
-		melNode,
+		snareLen = (n32 * 1) * sr,
+		snareBuf = actx.createBuffer(1, snareLen, sr),
+		snareData = snareBuf.getChannelData(0),
 
 		delayNode = actx.createDelay(),
 		delayNode2 = actx.createDelay(),
 		delayGain = actx.createGain();
 
+	// Hi-hat delay
 	delayNode.delayTime.value = n16;
 	delayNode2.delayTime.value = n16 + n32;
 	delayGain.gain.value = 0.1;
 
+	// Sine wave generator
 	var sfreq = 60,
 		freqDrop = (sfreq - 40) / kickData.length;
-
 	for (var i = 0; i < kickData.length; ++i) {
 		kickData[i] = Math.sin(i / (sr / (sfreq * 2 * Math.PI)));
 		sfreq -= freqDrop;
 	}
 
-	for (i = 0; i < melData.length; i++) {
-		melData[i] = (Math.random() * 2 - 1) / 2;
+	// Noise generator
+	for (i = 0; i < snareData.length; i++) {
+		snareData[i] = (Math.random() * 2 - 1) / 2;
 	}
 
 	/* Synth */
-	var synLen = (n8 * 4) * sr,
+	var synLen = (n8 * 8) * sr,
 		synBuf = actx.createBuffer(1, synLen, sr),
 		synBuf2 = actx.createBuffer(1, synLen, sr),
 		synData = synBuf.getChannelData(0),
-		synData2 = synBuf2.getChannelData(0),
-		synNode;
+		synData2 = synBuf2.getChannelData(0);
 
+	// Square wave generator
 	for (i = 0; i < synData.length; i++) {
-		synData[i] = Math.sin(i / (sr / (40.15 * 2 * Math.PI))) > 0 ? 0.5 : -0.5;
-		synData2[i] = Math.sin(i / (sr / (39.85 * 2 * Math.PI))) > 0 ? 0.5 : -0.5;
+		synData[i] = Math.sin(i / (sr / (40.15 * 2 * Math.PI))) > 0 ? 1 : -1;
+		synData2[i] = Math.sin(i / (sr / (39.85 * 2 * Math.PI))) > 0 ? 1 : -1;
 	}
 
 	var Env = function (a, d, r) {
@@ -74,24 +75,18 @@
 		}
 	};
 
-	kickNode = actx.createBufferSource()
-	kickNode.buffer = kickBuffer;
-	kickNode.loop = true;
-	kickNode.start(0);
+	function createNode(buffer) {
+		var node = actx.createBufferSource();
+		node.buffer = buffer;
+		node.loop = true;
+		node.start(0);
+		return node;
+	}
 
-	melNode = actx.createBufferSource()
-	melNode.buffer = melBuf;
-	melNode.loop = true;
-	melNode.start(0);
-
-	synNode = actx.createBufferSource();
-	synNode.buffer = synBuf;
-	synNode.loop = true;
-	synNode.start(0);
-	synNode2 = actx.createBufferSource();
-	synNode2.buffer = synBuf2;
-	synNode2.loop = true;
-	synNode2.start(0);
+	var kickNode = createNode(kickBuffer),
+		snareNode = createNode(snareBuf),
+		synNode = createNode(synBuf),
+		synNode2 = createNode(synBuf2);
 
 	// var i = "";
 	// for(var t in actx) {
@@ -106,18 +101,18 @@
 	kickEnv.inp(kickNode);
 	kickEnv.outp(gainMaster);
 
-	  var filter = actx.createBiquadFilter();
-	  filter.type = 0;
-	  filter.Q.value = 4;
-	  filter.frequency.value = 2000;
+	var hihatFilter = actx.createBiquadFilter();
+	hihatFilter.type = 0;
+	hihatFilter.Q.value = 4;
+	hihatFilter.frequency.value = 2000;
 
-	var melEnv = Env(0.001, 0.08, 0.2);
-	melEnv.inp(melNode);
-	melEnv.outp(filter);
+	var snareEnv = Env(0.001, 0.08, 0.2);
+	snareEnv.inp(snareNode);
+	snareEnv.outp(hihatFilter);
 
 	var hatEnv = Env(0.001, 0.01, 0.03);
-	hatEnv.inp(melNode);
-	hatEnv.outp(filter);
+	hatEnv.inp(snareNode);
+	hatEnv.outp(hihatFilter);
 
 
 	delayNode.connect(delayNode2);
@@ -128,21 +123,6 @@
 	synEnv.inp(synNode);
 	synEnv.inp(synNode2);
 
-function createSineWaveArray(durationSeconds, freqHz, amplitude, sampleRate)
-{
-    var length = timeToSampleFrame(durationSeconds, sampleRate);
-    var signal = new Float32Array(length);
-    var omega = 2 * Math.PI * freqHz / sampleRate;
-    var halfAmplitude = amplitude / 2;
-
-    for (var k = 0; k < length; ++k) {
-        signal[k] = halfAmplitude + halfAmplitude * Math.sin(omega * k);
-    }
-
-    return signal;
-}
-
-
 	var synFilter = actx.createBiquadFilter();
 	synFilter.type = 0;
 	synFilter.Q.value = 10;
@@ -150,17 +130,16 @@ function createSineWaveArray(durationSeconds, freqHz, amplitude, sampleRate)
 	var c = actx.currentTime;
 
 	function addWah(off) {
-		synFilter.frequency.setValueAtTime(1800, off + n8);
-		synFilter.frequency.exponentialRampToValueAtTime(200, off + n8 + n8 + 0.1);
-		synFilter.frequency.setValueAtTime(1800, off + n8 + n8+ 0.2);
-		synFilter.frequency.exponentialRampToValueAtTime(200, off + n8 + n8 + 0.3);
-		synFilter.frequency.setValueAtTime(1800, off + n8 + n8+ 0.4);
-		synFilter.frequency.exponentialRampToValueAtTime(200, off + n8+ n8 + 0.5);
-		synFilter.frequency.setValueAtTime(700, off + n8+ n8 + 0.6);
-		synFilter.frequency.exponentialRampToValueAtTime(1300, off + n8 + n8+ 0.9);
+		var freq = synFilter.frequency;
+		freq.setValueAtTime(1800, off + n8);
+		freq.exponentialRampToValueAtTime(200, off + n8 + n8 + 0.1);
+		freq.setValueAtTime(1800, off + n8 + n8+ 0.2);
+		freq.exponentialRampToValueAtTime(200, off + n8 + n8 + 0.3);
+		freq.setValueAtTime(1800, off + n8 + n8+ 0.4);
+		freq.exponentialRampToValueAtTime(200, off + n8+ n8 + 0.5);
+		freq.setValueAtTime(700, off + n8+ n8 + 0.6);
+		freq.exponentialRampToValueAtTime(1300, off + n8 + n8+ 0.9);
 	}
-
-
 
 	var synGain = actx.createGain();
 	synGain.gain.value = 0.6;
@@ -170,34 +149,37 @@ function createSineWaveArray(durationSeconds, freqHz, amplitude, sampleRate)
 	synGain.connect(gainMaster);
 
 	hatEnv.outp(delayNode);
-	filter.connect(gainMaster);
+	hihatFilter.connect(gainMaster);
 
 	gainMaster.connect(actx.destination);
 
 
-
+	// Sequence up some stuffs
 	for (var i = 0; i < 24; i++) {
-		addWah(c + (i * n8 * 4));
-		//addWah(c + (i * n8 * 4) + (n8 + n8));
+		// Wahhhh filter
+		(i > 3 && i % 2 === 0) && addWah(c + (i * n8 * 4)) ;
+		// Synth
+		synEnv.fire(c + (1 + i * 4) * n8 - n32 - n32);
 
-		synEnv.fire(c + (1 + i * 4) * n8);
-		hatEnv.fire(c + (1 + i * 4) * n8 + n32);
+		// Four on the floor
+		for (var j = 0; j < 4; j++) {
+			kickEnv.fire(c + ((j+1) + i * 4) * n8);
+		}
 
-		kickEnv.fire(c + (1 + i * 4) * n8);
+		// Hats n snare
+		if (i > 3) {
+			hatEnv.fire(c + (1 + i * 4) * n8 + n32);
 
-		kickEnv.fire(c + (2 + i * 4) * n8);
-		melEnv.fire(c + (2 + i * 4) * n8);
+			snareEnv.fire(c + (2 + i * 4) * n8);
 
-		hatEnv.fire(c + (2 + i * 4) * n8 + n16 + n32);
+			hatEnv.fire(c + (2 + i * 4) * n8 + n16 + n32);
 
-		kickEnv.fire(c + (3 + i * 4) * n8);
 
-		hatEnv.fire(c + (3 + i * 4) * n8 + n16);
+			hatEnv.fire(c + (3 + i * 4) * n8 + n16);
 
-		kickEnv.fire(c + (4 + i * 4) * n8);
-		melEnv.fire(c + (4 + i * 4) * n8);
-
-		hatEnv.fire(c + (4 + i * 4) * n8 + n16);
+			snareEnv.fire(c + (4 + i * 4) * n8);
+		}
+		i > 2 && hatEnv.fire(c + (4 + i * 4) * n8 + n16);
 	}
 
 
